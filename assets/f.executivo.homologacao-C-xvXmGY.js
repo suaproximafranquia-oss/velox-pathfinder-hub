f49d5537b02c115effde4712e7bc97fbbfad80d0
suaproximafranquia-oss <suaproximafranquia@gmail.com>
2026-09-14 14:20:15 +0000

    Deploying to stable-website from @ suaproximafranquia-oss/velox-pathfinder-hub@2b478b9aa5fef8f023c193d34285e5d75a02fa87 🚀

--- assets/f.executivo.homologacao-C-xvXmGY.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-D-uI9-NZ.js";import{V as t}from"./index-CdnO4_wG.js";import{t as n}from"./workspace-resource-guard-Bfl98hwJ.js";var r=e(),i=()=>(0,r.jsx)(n,{resource:`homologacao`,children:(0,r.jsx)(t,{})});export{i as component};
